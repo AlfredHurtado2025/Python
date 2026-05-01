511f93cb7cce40c2d910c7181814f559d2c24d2c
AlfredHurtado2024 <alfredhurtado04@gmail.com>
2026-05-01 19:26:19 -0400

    new update

---
+++ test4.py
@@ -0,0 +1,11 @@
+# first = input("What is your first and last name?: ")
+# print(f"Your full name is {first}")
+
+print("Welcome to this program!")
+
+pet = input("Please enter your pet name: ")
+city = input("Please enter your home city: ")
+
+print(f"Your new Twitter handle and bio: @Cyber{pet} from {city}!")
+
+
